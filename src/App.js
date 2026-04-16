693e7513af383a745f21eb6efb689f9f2b3c832e
Shemeera Fonseka <shemeerafonseka@gmail.com>
2026-04-16 22:37:25 +0530

    New navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-
-import './App.css';
-import About from './components/About/About';
-import Career from './components/Career/Career';
-import Home from './components/Home/Home';
-import Testimonials from './components/Testimonials/Testimonials';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import Touch from './components/Touch/Touch';
-import Work from './components/Work/Work';
-import Cv from './components/CV/Cv';
-import Navbar from './components/Navigation/Navbar';
-import Aboutme from './components/Aboutme/Aboutme';
-
+import "./App.css";
+import About from "./components/About/About";
+import Career from "./components/Career/Career";
+import Home from "./components/Home/Home";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import Touch from "./components/Touch/Touch";
+import Work from "./components/Work/Work";
+import Navbar from "./components/Navigation/Navbar";
+import Aboutme from "./components/Aboutme/Aboutme";
 
 function App() {
   return (
     <div>
-     <Navbar/> <Home/><Aboutme/><About/><Career/><Work/><Touch/>
+      <Navbar /> <Home />
+      <Aboutme />
+      <About />
+      <Career />
+      <Work />
+      <Touch />
     </div>
   );
 }
